9a6b076756b6b4d5a11a734be533e5cb5d115031
GustavNK <gustav.knudsen96@gmail.com>
2020-04-20 10:30:38 +0200

    lavet noget

--- Src/6/reset_logic.vhd
+++ Src/6/reset_logic.vhd
@@ -5,10 +5,10 @@ entity reset_logic is
 port(
 	-- Input
 	reset_in 	: in std_logic;
-	hrs_bin1		: in std_logic;
+	hrs_bin1	: in std_logic;
 	hrs_bin10	: in std_logic;
 	--Output
-	reset_out 	: out std_logic
+	reset_out	: out std_logic
 );
 end;
 
@@ -16,7 +16,7 @@ architecture arch of reset_logic is
 begin
 	process(reset_in, hrs_bin1, hrs_bin10)
 	begin
-		if reset_in = '0' OR (hrs_bin1 = '1' AND hrs_bin10 = '1') then	
+		if reset_in = '0' OR (hrs_bin1 = '1' AND hrs_bin10 = '1') then
 			reset_out <= '0';
 		else
 			reset_out <= '1';
